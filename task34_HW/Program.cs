125543123319ef3c44055e7da2046c83dc333e76
Sandaime-As <asenov1998@mail.ru>
2022-10-17 00:41:05 +0600

    task34 HomeWork

---
+++ task34_HW/Program.cs
@@ -0,0 +1,34 @@
+﻿/* Задайте массив заполненный случайными положительными трёхзначными числами. 
+Напишите программу, которая покажет количество чётных чисел в массиве.
+
+[345, 897, 568, 234] -> 2 */
+
+using static System.Console;
+Clear();
+Write("Введите размер массива: ");
+int length=int.Parse(ReadLine()!);
+int[] array=GetArray(length);
+WriteLine($"[{String.Join(" ",array)}]");          
+
+
+int evenNumbers=0;
+foreach (int element in array)
+{
+    //WriteLine(element%2);
+    if(element%2==0)
+    {
+        evenNumbers++; 
+    }
+}
+WriteLine($"Колличество четных чисел в массиве: {evenNumbers}"); 
+
+int[] GetArray(int size)
+{
+    Random rnd=new Random();
+    int[] result = new int[size];
+    for (int i = 0; i < result.Length; i++)
+    {
+        result[i]=rnd.Next(100, 1000-1);
+    }
+    return result;
+}
